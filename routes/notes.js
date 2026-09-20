81547feccfccb931175f4ba5e9e0cf129e87a245
Abdul Hameed <abdulhameedinfo349@gmail.com>
2026-09-20 16:33:58 +0500

    Add Get by Id and GetAll methods

--- routes/notes.js
+++ routes/notes.js
@@ -24,4 +24,24 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/", async (req, res) => {
+
+    const notes = await Note.find();
+
+    res.json(notes);
+});
+
+router.get("/:id", async (req, res) => {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+        return res.status(404).json({
+            message: "Note not found"
+        });
+    }
+
+    res.json(note);
+});
+
+
 export default router;
